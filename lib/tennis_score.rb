1bcf7edd4dff81ba5a5cb0136bd14ca3950db5f3
benilovj <benilov@gmail.com>
2011-11-22 00:53:40 +0100

    reqs implemented

--- lib/tennis_score.rb
+++ lib/tennis_score.rb
@@ -8,10 +8,14 @@ class Game
   end
 
   def score
-    case
-    when no_points_played? then "Love all"
-    else "#{decode_for(@server)}, #{decode_for(@receiver)}".capitalize
+    value = case
+    when no_points_played? then "love all"
+    when game_won? then "game, #{player_in_front}"
+    when (in_deuce? and scores_even?) then "deuce"
+    when in_deuce? then "advantage, #{player_in_front}"
+    else "#{decode_for(@server)}, #{decode_for(@receiver)}"
     end
+    value[0].upcase + value[1..-1]
   end
 
   def point_to(player)
@@ -19,7 +23,39 @@ class Game
     self
   end
 
+  def with_points_to(*players)
+    players.inject(self) {|game, winner| game.point_to(winner)}
+  end
+
   protected
+  def game_won?
+    game_won_normally? or game_won_in_deuce?
+  end
+
+  def game_won_normally?
+    not scores_even? and not in_deuce? and @points_won_by[player_in_front] == 4
+  end
+
+  def game_won_in_deuce?
+    in_deuce? and not scores_even? and gap_between_scores > 1
+  end
+
+  def in_deuce?
+    @points_won_by.values.all? {|v| v >= 3}
+  end
+
+  def player_in_front
+    @points_won_by.keys.sort_by {|key| @points_won_by[key]}.last
+  end
+
+  def gap_between_scores
+    @points_won_by.values.max - @points_won_by.values.min
+  end
+
+  def scores_even?
+    @points_won_by[@server] == @points_won_by[@receiver]
+  end
+
   def no_points_played?
     @points_won_by[@server] == 0 and @points_won_by[@receiver] == 0
   end
--- spec/tennis_score_spec.rb
+++ spec/tennis_score_spec.rb
@@ -17,12 +17,31 @@ RSpec::Matchers.define :have_a_score_of do |expected_score|
 end
 
 describe Game do
-  subject {Game.new("J", "B")}
-  
-  it { should have_a_score_of("Love all") }
-  it { should have_a_score_of("Fifteen, love").after_points_won_by("J") }
-  it { should have_a_score_of("Love, fifteen").after_points_won_by("B") }
-  it { should have_a_score_of("Thirty, love").after_points_won_by("J", "J") }
-  it { should have_a_score_of("Thirty, fifteen").after_points_won_by("J", "J", "B") }
-  it { should have_a_score_of("Forty, fifteen").after_points_won_by("J", "J", "B", "J") }
+  context "normal game" do
+    subject {Game.new("J", "B")}
+
+    it { should have_a_score_of("Love all") }
+    it { should have_a_score_of("Fifteen, love").after_points_won_by("J") }
+    it { should have_a_score_of("Love, fifteen").after_points_won_by("B") }
+    it { should have_a_score_of("Thirty, love").after_points_won_by("J", "J") }
+    it { should have_a_score_of("Thirty, fifteen").after_points_won_by("J", "J", "B") }
+    it { should have_a_score_of("Forty, fifteen").after_points_won_by("J", "J", "B", "J") }
+  end
+
+  context "deuce game" do
+    subject { Game.new("J", "B").with_points_to("J", "J", "B", "J", "B", "B")}
+    
+    it { should have_a_score_of("Deuce") }
+    it { should have_a_score_of("Advantage, J").after_points_won_by("J") }
+  end
+
+  context "game won normally" do
+    subject {Game.new("J", "B").with_points_to("J", "J", "J", "J")}
+    it { should have_a_score_of("Game, J") }
+  end
+
+  context "game won from deuce" do
+    subject {Game.new("J", "B").with_points_to("J", "J", "J", "B", "B", "B", "B", "B")}
+    it { should have_a_score_of("Game, B") }
+  end
 end
